30feaa555c9cb73ffa5b72fdaa6d0667ba4973f1
leejaelll <jayaom91076@gmail.com>
2022-09-27 17:56:33 +0900

    js: 클릭 이벤트 핸들러 함수 추가

---
+++ public/js/button.js
@@ -0,0 +1,44 @@
+const opacityButtons = [...getAll(".button--opacity")];
+
+opacityButtons.forEach(($button, i) => {
+  const clickHandler = (e) => {
+    opacityButtons[i].classList.toggle("is--active");
+    if (opacityButtons[i].classList.contains("is--active")) {
+      e.target.innerHTML = `<span
+      class="icon icon--bell--empty"
+      aria-hidden="true"
+    ></span>
+    알림받는 중`;
+    } else {
+      e.target.innerHTML = `<span
+      class="icon icon--bell--empty"
+      aria-hidden="true"
+    ></span>
+    알림받기`;
+    }
+  };
+
+  $button.addEventListener("click", clickHandler);
+});
+
+const fatButtons = [...getAll(".button--fat")];
+
+fatButtons.forEach(($button, i) => {
+  const clickHandler = (e) => {
+    console.log(e.target.innerHTML);
+    fatButtons[i].classList.toggle("is--active");
+    if (fatButtons[i].classList.contains("is--active")) {
+      e.target.innerHTML = `<span
+      class="icon icon--arrow"
+      aria-hidden="true"
+    ></span>
+      <span>팔로잉</span>
+    `;
+    } else {
+      e.target.innerHTML = ` <span class="icon icon--plus"></span>
+      <span>팔로우</span>`;
+    }
+  };
+
+  $button.addEventListener("click", clickHandler);
+});
