02f7b8dc9c62f35f0eb5ac8013502e33c759876f
Seonu <seonup95@gmail.com>
2022-09-28 13:24:16 +0900

    js: lineup 섹션 aria-* 스크립트 완료

--- public/js/lineup.js
+++ public/js/lineup.js
@@ -1,23 +1,41 @@
 const dayList = get(".day__list");
-const dayItem = getAll(".day__item");
-const selectDay = getAll(".daySelect");
 
-function handelList(event) {
+const handelLinupList = (event) => {
+  if (event.target === "BUTTON") return;
+
+  const lineupTabPanel = get("#lineupTabpanel");
+  const lineupCardItem = getAll(".lineup .card__item");
+  const dayButtons = getAll(".day__list button");
   const target = event.target;
-  const remove = get(".is--active");
-  if (target.nodeName === "BUTTON") {
-    const targetDay = target.dataset.filter;
-    const dayArray = getAll(`[data-${targetDay}="true"`);
-    selectDay.forEach((item) => {
-      item.classList.remove(IS_SHOW);
-      item.classList.add(IS_HIDDEN);
-    });
-    dayArray.forEach((item) => {
-      if (item.classList.contains(IS_HIDDEN)) item.classList.remove(IS_HIDDEN);
-      item.classList.add(IS_SHOW);
-    });
-    remove.classList.remove(IS_ACTIVE);
-    target.classList.add(IS_ACTIVE);
-  }
-}
-dayList.addEventListener("click", handelList);
+  const targetDay = target.id;
+  const dayArray = getAll(`[data-${targetDay}="true"`);
+
+  lineupTabPanel.setAttribute("aria-labelledby", `${targetDay}`);
+  lineupTabPanel.setAttribute("aria-live", true);
+
+  lineupCardItem.forEach((item) => {
+    item.classList.remove(IS_SHOW);
+    item.classList.add(IS_HIDDEN);
+  });
+
+  dayButtons.forEach((button) => {
+    if (button !== target) {
+      button.setAttribute("aria-selected", false);
+      button.setAttribute("aria-live", false);
+      button.classList.remove(IS_ACTIVE);
+    } else {
+      button.setAttribute("aria-live", true);
+      button.setAttribute("aria-selected", true);
+      button.classList.add(IS_ACTIVE);
+    }
+  });
+
+  dayArray.forEach((item) => {
+    if (item.classList.contains(IS_HIDDEN)) item.classList.remove(IS_HIDDEN);
+    item.classList.add(IS_SHOW);
+  });
+  target.classList.add(IS_ACTIVE);
+  target.setAttribute("aria-selected", true);
+};
+
+dayList.addEventListener("click", handelLinupList);
